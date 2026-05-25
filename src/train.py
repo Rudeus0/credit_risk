3737ed97cce0660db58a4d059c5bbdfc4a46afcc
Ruthresh <rtrwaran5@gmail.com>
2026-05-25 18:35:40 +0530

    feat: add credit_model function with LGR, RFC, and XGB baselines

--- src/train.py
+++ src/train.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+
 
 def data_load()-> pd.DataFrame:
     credit = pd.read_csv("../data/credit_customers.csv")
@@ -29,6 +34,16 @@ def transform_scaler(credit_dum: pd.DataFrame)-> tuple:
     return X_train, X_test, y_train, y_test, X_train_scaled, X_test_scaled
     
      
+def credit_model(X_train_scaled: np.ndarray, y_train: pd.Series ) -> tuple:
+  
+    model_lgr = LogisticRegression()
+    model_lgr.fit(X_train_scaled, y_train)
+    
+    model_rfc = RandomForestClassifier()
+    model_rfc.fit(X_train_scaled, y_train)
+    
+    model_xgb = XGBClassifier()
+    model_xgb.fit(X_train_scaled, y_train)
+    
+    return model_lgr, model_rfc, model_xgb
 
-    
-    
