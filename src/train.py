ee827ea3ab0817be11a8408d882355b823c156db
Ruthresh <rtrwaran5@gmail.com>
2026-05-25 16:21:03 +0530

    feat: add features() with target encoding and get_dummies

--- src/train.py
+++ src/train.py
@@ -4,3 +4,13 @@ def data_load()-> pd.DataFrame:
     credit = pd.read_csv("../data/credit_customers.csv")
     return credit
 
+def features(credit: pd.DataFrame)-> pd.DataFrame:
+    credit['class'] = credit['class'].map({"good": 0, "bad": 1})
+    categorical_cols = credit.select_dtypes(include=["object", "str"]).columns.to_list()
+    
+    if 'class' in categorical_cols:
+        categorical_cols.remove('class')
+        
+    credit_dum = pd.get_dummies(credit, columns=categorical_cols, drop_first=True)
+    
+    return credit_dum
